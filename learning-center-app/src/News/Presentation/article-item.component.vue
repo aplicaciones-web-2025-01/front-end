b1ee2a83eebe7eb4383013c515b68b08376c727b
NALDO REUPO MUSAYON G <pcsinreu@upc.edu.pe>
2025-04-25 18:52:35 -0500

    feat: format files

--- learning-center-app/src/News/Presentation/article-item.component.vue
+++ learning-center-app/src/News/Presentation/article-item.component.vue
@@ -1,10 +1,10 @@
 ﻿<script setup>
-import {Article} from "@/News/Domain/article.entity.js"
 import {defineEmits, defineProps} from "vue";
 
 const props = defineProps({
-  article: {type: Article, required: true}
+  article: {type: Object, required: true}
 });
+
 
 const emits = defineEmits(["show-tooltip"]);
 
@@ -33,26 +33,8 @@ async function shareArticle() {
 
 <template>
   <pv-card class="m-2">
-    <template #header>
-      <img :alt="article.title" :src="article.urlToImage" class="image-fit"/>
-    </template>
     <template #title>
       <p>{{ article.title }}</p>
-    </template>
-    <template #subtitle>
-      <p class="flex align-content-start flex-wrap">
-        <span class="flex align-items-center justify-content-center mr-2">
-          <pv-avatar :aria-label="article.source.name"
-                     :image="article.source.urlToLogo"
-                     shape="circle"/>
-        </span>
-        <span class="flex align-items-center justify-content-center">
-          {{ article.source.name }}
-        </span>
-      </p>
-      <p class="flex align-content-start flex-wrap">
-        <span class="text-sm">{{ article.getFormatedPublishedAt() }}</span>
-      </p>
     </template>
     <template #content>
       <p class="flex align-content-start flex-wrap mt-4">
