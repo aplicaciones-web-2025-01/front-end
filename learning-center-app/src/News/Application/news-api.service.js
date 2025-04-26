5a34a2f48da5216faf8e402fc3d8ffb79943fb3c
NALDO REUPO MUSAYON G <pcsinreu@upc.edu.pe>
2025-04-25 19:55:37 -0500

    feat : add env

--- learning-center-app/src/News/Application/news-api.service.js
+++ learning-center-app/src/News/Application/news-api.service.js
@@ -1,10 +1,15 @@
 ﻿import axios from "axios";
 
+const newApi = import.meta.env.VITE_NEWS_API_URL;
+const headLines = import.meta.env.VITE_TOP_HEADLINES_ENDPOINT_PATH;
+const key = import.meta.env.VITE_NEWS_API_KEY
+
 export class NewsApiService {
 
     async getArticles() {
-        var response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=fecf4feeffa64e4da682e7d268612ce5')
+        var response = await axios.get(`${newApi}/${headLines}?country=us&apiKey=${key}`)
         return response.data.articles;
     }
+
 }
 
