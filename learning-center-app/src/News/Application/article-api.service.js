25de832c0ca2b8469c7a01d67948dc4078d3c252
NALDO REUPO MUSAYON G <pcsinreu@upc.edu.pe>
2025-05-08 21:31:10 -0500

    feat: hardcode url

--- learning-center-app/src/News/Application/article-api.service.js
+++ learning-center-app/src/News/Application/article-api.service.js
@@ -1,32 +1,35 @@
 ﻿import axios from "axios";
 
 const newApi = import.meta.env.VITE_NEWS_API_URL;
-const headLines = import.meta.env.VITE_TOP_ARTICLE_ENDPOINT_PATH;
-const key = import.meta.env.VITE_NEWS_API_KEY
+//const headLines = import.meta.env.VITE_TOP_ARTICLE_ENDPOINT_PATH;
+
+const baseUrl ="https://66f221c24153791915531f3c.mockapi.io";
+const headLines ="articles";
 
 export class ArticleService {
     async getAll() {
-        var response = await axios.get(`${newApi}/${headLines}`)
+        console.log("newApi",newApi)
+        var response = await axios.get(`${baseUrl}/${headLines}`)
         return response;
     }
 
     async getById(id) {
-        var response = await axios.get(`${newApi}/${headLines}/${id}`)
+        var response = await axios.get(`${baseUrl}/${headLines}/${id}`)
         return response;
     }
 
     async createArticle(article) {
-        var response = await axios.post(`${newApi}/${headLines}`, article)
+        var response = await axios.post(`${baseUrl}/${headLines}`, article)
         return response;
     }
 
     async updateArticle(id, article) {
-        var response = await axios.put(`${newApi}/${headLines}/${id}`, article)
+        var response = await axios.put(`${baseUrl}/${headLines}/${id}`, article)
         return response;
     }
 
     async deleteArticle(id) {
-        var response = await axios.delete(`${newApi}/${headLines}/${id}`)
+        var response = await axios.delete(`${baseUrl}/${headLines}/${id}`)
         return response;
     }
 }
